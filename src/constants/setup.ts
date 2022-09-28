eca19cda55cc7d306a12d217aa1d53b7f77dcd81
Erik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-09-28 13:17:17 -0300

    feat: add setup to constants

---
+++ src/constants/setup.ts
@@ -0,0 +1,30 @@
+const setup = [
+  {
+    title: "Notebook HP 1000 14”",
+    description: "i5 3° gen, 8 RAM, 500 GB",
+    url: ""
+  },
+  {
+    title: "Monitor",
+    description: "TV LG 29” - Support Monitor",
+    url: "https://www.lg.com/br/monitores/lg-29LN300B-PC"
+  },
+  {
+    title: "Mic",
+    description: "BM800 Giant",
+    url: "https://produto.mercadolivre.com.br/MLB-1978445240-microfone-estudio-bm800-usb-aranha-braco-pop-filter-_JM?matt_tool=88695644&matt_word=&matt_source=google&matt_campaign_id=14300459461&matt_ad_group_id=126793517756&matt_match_type=&matt_network=g&matt_device=c&matt_creative=543111991139&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=109549604&matt_product_id=MLB1978445240&matt_product_partition_id=1415060497196&matt_target_id=aud-329638142615:pla-1415060497196&gclid=CjwKCAjw4c-ZBhAEEiwAZ105RZlQd7uFd-sQ2oYAz0RXKqZjXjZRnV7uqTUTSeQf2g55n2hZ-aSffhoC6uEQAvD_BwE"
+  }
+  ,
+  {
+    title: "Mouse",
+    description: "HP Gaming Mouse M160",
+    url: "https://www.amazon.com.br/MOUSE-GAMER-HP-MULTICORES-PRETO/dp/B0827JHZN9/ref=asc_df_B0827JHZN9/?tag=googleshopp00-20&linkCode=df0&hvadid=379708004298&hvpos=&hvnetw=g&hvrand=2045383884092739590&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=20089&hvtargid=pla-906126860391&psc=1"
+  },
+  {
+    title: "Notebook Stand",
+    description: "ST35031 Black - Masticmol",
+    url: "https://www.amazon.com.br/SUPORTE-PARA-NOTEBOOK-ST35031-PRETO/dp/B075DWMGDK/ref=sr_1_11?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=20KKDZC1QEF54&keywords=Notebook+Stand+plastico&qid=1664375522&s=electronics&sprefix=notebook+stand+plastico%2Celectronics%2C249&sr=1-11"
+  }
+]
+
+export { setup }
