d3b50e229a52c449b30238fc722a121a66e4a951
Èrik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-10-01 22:45:12 -0300

    fix: icon name always undefined

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,3 +1,4 @@
+import { ReactElement } from 'react'
 import * as Ai from 'react-icons/ai'
 
 type Icon = {
@@ -5,18 +6,14 @@ type Icon = {
 }
 
 const Icon: React.FC<Icon> = ({ name }) => {
-  const aiIcons = Object.values(Ai)
+  const icons: { [key: string]: ReactElement } = {
+    Linkedin: <Ai.AiFillLinkedin size={24} color="#848484" />,
+    Github: <Ai.AiFillGithub size={24} color="#848484" />,
+    Twitter: <Ai.AiFillTwitterSquare size={24} color="#848484" />,
+    Instagram: <Ai.AiFillInstagram size={24} color="#848484" />,
+  }
 
-  const icons = aiIcons.filter((_, i) =>
-    aiIcons[i].name.includes(`AiFill${name}`)
-  )
-
-  const SimpleIcon = icons[0]
-  const IconSquare = icons[1]
-
-  if (IconSquare) return <IconSquare size={24} color="#848484" />
-
-  return <SimpleIcon size={24} color="#848484" />
+  return icons[name]
 }
 
 export { Icon }
